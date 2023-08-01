cdcee744029819d4bb7c7a97fe4a143cda08a97b
Guillermo Solovey <gsolovey@gmail.com>
2023-08-01 16:09:47 -0300

    primer commit

---
+++ R/sismos.R
@@ -0,0 +1,10 @@
+#' Primavera Dataset
+#'
+#' A sample dataset containing some data.
+#'
+#' @format A data frame with n rows and m columns.
+#' @usage data(sismos)
+#' @name sismos
+#' @docType data
+#' @keywords datasets
+NULL
